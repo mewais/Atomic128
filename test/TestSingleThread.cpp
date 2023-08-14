e325a60243391b9096d37ce02a16d003ce1bae45
Mohammad Ewais <mohammad.a.ewais@gmail.com>
2023-08-14 16:58:39 -0400

    Adding functionality test

---
+++ test/TestSingleThread.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdint>
+
+#include "Atomic128.hpp"
+
+struct alignas(16) TestData
+{
+    uint32_t first;
+    uint32_t second;
+    uint64_t third;
+
+    friend std::ostream& operator<<(std::ostream& os, const TestData& data)
+    {
+        os << "first: " << data.first << ", second: " << data.second << ", third: " << data.third;
+        return os;
+    }
+};
+
+int main()
+{
+    A128::Atomic128<TestData> data(0, 0, 0);
+
+    // Initialize the data
+    TestData init_data = {42, 99, 123456789ULL};
+    TestData new_data = {101, 202, 987654321ULL};
+    TestData result_data;
+
+    // Test Load
+    result_data = data.Load();
+    std::cout << "Initial data: " << result_data << std::endl;
+
+    // Test Store and Load
+    data.Store(init_data);
+    result_data = data.Load();
+    std::cout << "Stored data: " << result_data << std::endl;
+
+    // Test Exchange
+    result_data = data.Exchange(new_data);
+    std::cout << "Exchanged data: " << result_data << ", Now holds: " << data.Load() << std::endl;
+
+    // Test CompareExchange
+    bool success = data.CompareExchange(new_data, init_data);
+    if (success)
+    {
+        std::cout << "CompareExchange success, now holds " << data.Load() << std::endl;
+    }
+    else
+    {
+        std::cout << "CompareExchange failed, expected: " << init_data << ", found: " << data.Load() << std::endl;
+    }
+}
